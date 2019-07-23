6d3a78fd5a10201d8742423b55271daee7dfabef
domhnall <fiopwk@gmail.com>
2019-07-23 05:06:13 -0400

    Create Cumulator.js
    elaborate what you find and explain logic

---
+++ Cumulator.js
@@ -0,0 +1,11 @@
+const obj = {X: 10, V: 5, L: 50, M: 1000, I: 1, Y: 5000, C: 100, D: 500};
+let keys = Object.keys(obj)
+let value = Object.values(obj)
+
+let name = 'domhnall flopwiki fiopwk github';
+
+function cumulator () {keys.forEach (function(k, v) { if (name .toUpperCase() .includes(k)) {
+  console.log ( 'If string has',name .toUpperCase() .split(k).length - 1, k, 'and', k, '=', value[v],'then', name .toUpperCase() .split(k).length - 1,'*',value[v],'=', (name .toUpperCase() .split(k).length - 1) * value[v]) };})
+  };
+
+cumulator();
