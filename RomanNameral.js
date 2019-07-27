ac562bd86db101f45cf9c659d16fe7f4192c0799
domhnall <fiopwk@gmail.com>
2019-07-27 06:18:11 -0400

    Update RomanNameral.js

--- RomanNameral.js
+++ RomanNameral.js
@@ -1,7 +1,7 @@
 const obj = {X: 10, M: 1000, I: 1, C: 100, D: 500, L: 50, V: 5, Y: 5000};
 
 let key = Object.keys(obj);
-let value  = Object.values(obj);
+let val  = Object.values(obj);
 let total = 0;
 
 
@@ -14,7 +14,7 @@ function subScore() {
     let score = 0;
     key .forEach(function(k, v) {
     if (name .toUpperCase() .includes(k)) { 
-      total = score += (name .toUpperCase() .split(k) .length - 1) * value[v];
+      total = score += (name .toUpperCase() .split(k) .length - 1) * val[v];
     
     }
   
