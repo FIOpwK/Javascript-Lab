a63f478a5cdc8956dec008b0c65632e759492186
domhnall <fiopwk@gmail.com>
2019-07-27 15:48:30 -0400

    Update RomanNameral.js

--- RomanNameral.js
+++ RomanNameral.js
@@ -1,26 +1,27 @@
-const obj = {X: 10, M: 1000, I: 1, C: 100, D: 500, L: 50, V: 5, Y: 5000};
+const obj = {
+  X: 10,
+  M: 1000,
+  I: 1,
+  C: 100,
+  D: 500,
+  L: 50,
+  V: 5,
+  Y: 5000
+};
 
-let key = Object.keys(obj);
-let val  = Object.values(obj);
+const key = Object.keys(obj);
+const val = Object.values(obj);
 let total = 0;
 
+let name = window.prompt("Enter your name to see its weight: ");
 
-//editable
-let name = 'RomanNameral';
-
-
-//using forEach() loop
 function subScore() {
-    let score = 0;
-    key .forEach(function(k, v) {
-    if (name .toUpperCase() .includes(k)) { 
-      total = score += (name .toUpperCase() .split(k) .length - 1) * val[v];
-    
+  let score = 0;
+  key.forEach(function(k, v) {
+    if (name.toUpperCase().includes(k)) {
+      total = score += (name.toUpperCase().split(k).length - 1) * val[v];
     }
-  
-  })
-
+  });
+  alert("Weight is " + total);
 }
 subScore();
-
-console.log('Weight is:', total)
